06b72d079da04c64cfef97b225fe8ad214146c83
Vetle Fredriksen <vetle.fredriksen@soprasterianorway.onmicrosoft.com>
2020-12-03 21:23:48 +0100

    Delete useless comments and prints, remove function part_two_trick()

--- 3/3.py
+++ 3/3.py
@@ -10,20 +10,12 @@ def part_one():
   trees=0
   x,y=0,0
   for line in grid:
-    print(y)
     if line[x] == '#':
-      print("tree")
       trees+=1
-      #line[x] = 'X'
-    else:
-      print("Not tree")
-      #line[x] = 'O'
     x+=3
     y+=1
     if x>30:
       x-=31
-  for line in grid:
-    print(line)
   print(trees)
 
 def part_two(x_slope, y_slope):
@@ -31,79 +23,23 @@ def part_two(x_slope, y_slope):
   trees=0
   x,y=0,0
   for line in grid:
-    #print(y)
-    if line[x] == '#':
-      #print("tree")
+    if line[x] == '#' and y%y_slope == 0:
       trees+=1
-      #line[x] = 'X'
-    else:
-      print("Not tree")
-      #line[x] = 'O'
-    x+=x_slope
+    if y%y_slope == 0:
+      x+=x_slope
     y+=1
     if x>30:
       x-=31
-  #for line in grid:
-    #print(line)
-  print(trees)
+  return trees
 
-def part_two_trick(x_slope, y_slope):
-  grid = readfile()
-  trees=0
-  x,y=0,0
-  for line in grid:
-    if y % 2 != 0:
-      fake = True
-    elif y % 2 == 0:
-      fake=False
-    #print(y)
-    if line[x] == '#' and not fake:
-      #print("tree")
-      trees+=1
-      #line[x] = 'X'
-    else:
-      print("Not tree")
-      #line[x] = 'O'
-    x+=x_slope
-    if fake:
-      x-=x_slope
-    y+=1
-    if x>30:
-      x-=31
-  #for line in grid:
-    #print(line)
-  print(trees)
 
-#def part_two(x_slope, y_slope):
-#  grid = readfile()
-#  trees=0
-#  x,y=0,0
-#  for line in grid:
-#    #print(y)
-#    if line[x] == '#':
-#      #print("tree")
-#      trees+=1
-#      #line[x] = 'X'
-#    else:
-#      continue
-#      print("Not tree")
-#      #line[x] = 'O'
-#    x+=x_slope
-#    y+=1
-#    if x>30:
-#      x-=31
-#  #for line in grid:
-#    #print(line)
-#  
-#  print("X slope = ", x_slope, " gives: ", trees, "trees")
-#
 def main():
   if int(sys.argv[1]) == 1:
     part_one()
   elif int(sys.argv[1]) == 2:
-    #part_two(1,1)
-    #part_two(3,1)
-    #part_two(5,1)
-    #part_two(7,1)
-    part_two_trick(1,2)
+    trees=[]
+    slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    for element in slopes:
+      trees.append((part_two(element[0], element[1])))
+    print(trees)  
 main()
